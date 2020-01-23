54c88ec269afc0d3907b5c228142c042377a9c1c
Marie-Carrere <marie.carrere@icloud.com>
2020-01-23 12:34:17 +0100

    :sparkles: Allow connection to database

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://admin:81LUUmfWCSXHo5CE@cluster0-mfw6l.mongodb.net/test?retryWrites=true&w=majority');
+mongoose.connection.once('open', () => {
+  console.log('Connected to database ✅');
+});
 
 app.use('/graphql', graphqlHTTP({
   schema,
