347d3d1f78bee8bd65460c9a2b705b31dbe858d2
Marie-Carrere <marie.carrere@icloud.com>
2020-01-29 15:52:49 +0100

    :recycle: Update resolve functions and Book component

---
+++ client/src/components/BookList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const BookList = () => (
+  <div>
+    <h1>Book List component</h1>
+    <ul>
+      <li>book title</li>
+    </ul>
+  </div>
+)
+
+export default BookList;
+
